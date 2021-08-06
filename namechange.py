2ca0430e213ce2f16ec3444e0b99884a5597243c
Dom <72845136+53z@users.noreply.github.com>
2021-08-06 23:11:36 +1000

    Update namechange.py

--- namechange.py
+++ namechange.py
@@ -4,6 +4,7 @@ import os
 from colorama import init, Style, Fore
 from datetime import datetime
 init()
+failedToLogin = True
 debugmode = False
 token = ''
 with open('.\\valid.txt', 'w+') as f:
@@ -44,21 +45,42 @@ def accessToken():
             print(response_json)
         pass
 
+def checkAccType():
+    global AccType
+    headers = {
+        "Authorization" : f"Bearer {token}"
+    }
+    r = requests.get("https://api.mojang.com/user", headers=headers)
+    if r.status_code == 200:
+        AccType = 'SFA'
+    else:
+        AccType = 'NFA'
+
+def getUsername():
+    global username
+    headers = {
+        "Authorization" : f"Bearer {token}"
+    }
+    r = requests.get("https://api.minecraftservices.com/minecraft/profile", headers=headers)
+    username = r.json()['name']
+
+
 def checkNameChange():
+    global namechange
     headers = {
         "Authorization" : f"Bearer {token}"
     }
     r = requests.get("https://api.minecraftservices.com/minecraft/profile/namechange", headers=headers)
     if failedToLogin != True:
         if r.json()['nameChangeAllowed'] == True:
-            print(f"{Fore.GREEN}[SUCCESS]{Style.RESET_ALL} | Account: {email} can namechange!")
+            namechange = True
             canlogin.append(email)
             with open('.\\valid.txt', 'a') as f:
                 f.write(f'{email}:{password}\n')
             if debugmode == True:
                 print(r.json())
         elif r.json()['nameChangeAllowed'] == False:
-            print(f"{Fore.RED}[FAIL]{Style.RESET_ALL} | Account: {email} cannot namechange!")
+            namechange = False
             if debugmode == True:
                 print(r.json())
         else:
@@ -67,10 +89,26 @@ def checkNameChange():
                 print(r.json())
     else:
         pass
+
+    r = requests.get("https://api.minecraftservices.com/minecraft/profile/namechange", headers=headers)
 with open('.\\accounts.txt', 'r') as f:
     for line in f:
         email, password = line.split(':')
         email = email.strip()
         password = password.strip()
         accessToken()
+        checkAccType()
+        getUsername()
         checkNameChange()
+        if namechange == True:
+            namechange = 'True'
+        else:
+            namechange = 'False'
+        if failedToLogin == False:
+            print(f"""
+Email: {email}
+Password: {password}
+Username: {username}
+Namechange: {namechange}
+Account Type: {AccType}
+            """)
