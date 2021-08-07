d309a9dbb8310cef6267ff312edab8b89055cc77
Dom <72845136+53z@users.noreply.github.com>
2021-08-07 18:23:06 +1000

    Added Proxies Functionality

--- namechange.py
+++ namechange.py
@@ -4,8 +4,9 @@ import os
 from colorama import init, Style, Fore
 from datetime import datetime
 init()
+proxynum = 0
 failedToLogin = True
-debugmode = False
+debugmode = False # Warning! Enabling This Will Expose Your Auth Keys, Tokens are Other Sensitive Info in the Console. Because of this, it is recommended you leave this off for normal usage.
 token = ''
 with open('.\\valid.txt', 'w+') as f:
     f.truncate()
@@ -17,7 +18,7 @@ if os.path.getsize('.\\accounts.txt') > 0:
 else:
     print(f"{Fore.RED}[FAIL]{Style.RESET_ALL} | Please enter accounts in proper format in accounts.txt file")
     os.abort()
-def accessToken():
+def accessToken(proxy):
     global token
     global failedToLogin
     payloads = {
@@ -30,7 +31,7 @@ def accessToken():
     headers = {
         "Content-Type" : "application/json"
     }
-    responseJSON = requests.post("https://authserver.mojang.com/authenticate", json=payloads, headers=headers)
+    responseJSON = requests.post("https://authserver.mojang.com/authenticate", json=payloads, headers=headers, proxies={'http' : f'{proxy}'})
     response_json = responseJSON.json()
     try:
         token = response_json['accessToken']
@@ -46,35 +47,24 @@ def accessToken():
             print(response_json)
         pass
 
-def checkAccType():
-    global AccType
-    headers = {
-        "Authorization" : f"Bearer {token}"
-    }
-    r = requests.get("https://api.mojang.com/user", headers=headers)
-    if r.status_code == 200:
-        AccType = 'SFA'
-    else:
-        AccType = 'NFA'
-
-def getUsername():
+def getUsername(proxy):
     global username
     headers = {
         "Authorization" : f"Bearer {token}"
     }
-    r = requests.get("https://api.minecraftservices.com/minecraft/profile", headers=headers)
+    r = requests.get("https://api.minecraftservices.com/minecraft/profile", headers=headers, proxies={'http' : f'{proxy}'})
     try:
         username = r.json()['name']
     except:
         username = 'Error Trying To Get Username'
 
 
-def checkNameChange():
+def checkNameChange(proxy):
     global namechange
     headers = {
         "Authorization" : f"Bearer {token}"
     }
-    r = requests.get("https://api.minecraftservices.com/minecraft/profile/namechange", headers=headers)
+    r = requests.get("https://api.minecraftservices.com/minecraft/profile/namechange", headers=headers, proxies={'http' : f'{proxy}'})
     if failedToLogin != True:
         if r.json()['nameChangeAllowed'] == True:
             namechange = True
@@ -94,26 +84,37 @@ def checkNameChange():
     else:
         pass
 
-    r = requests.get("https://api.minecraftservices.com/minecraft/profile/namechange", headers=headers)
+    r = requests.get("https://api.minecraftservices.com/minecraft/profile/namechange", headers=headers, proxies={'http' : f'{proxy}'})
 with open('.\\accounts.txt', 'r') as f:
     for line in f:
         email, password = line.split(':')
         email = email.strip()
         password = password.strip()
-        accessToken()
-        checkAccType()
-        if failedToLogin == False:
-            getUsername()
-        checkNameChange()
-        if namechange == True:
-            namechange = 'True'
-        else:
-            namechange = 'False'
-        if failedToLogin == False:
-            print(f"""
+        with open('.\\proxies.txt', 'r') as proxyfile:
+            lineproxy = proxyfile.readlines()[proxynum]
+            accessToken('http://' + lineproxy + '/')
+            if debugmode == True:
+                    print(f'Proxy Used: {lineproxy}')
+            proxynum = proxynum+1
+            if failedToLogin == False:
+                getUsername('http://' + lineproxy + '/')
+                if debugmode == True:
+                    print(f'Proxy Used: {lineproxy}')
+                proxynum = proxynum+1
+            checkNameChange('http://' + lineproxy + '/')
+            if debugmode == True:
+                    print(f'Proxy Used: {lineproxy}')
+            proxynum = proxynum+1
+            if failedToLogin == False:
+                if namechange == True:
+                    namechange = 'True'
+                else:
+                    namechange = 'False'
+            if failedToLogin == False:
+                print(f"""
 Email: {email}
 Password: {password}
 Username: {username}
 Namechange: {namechange}
-Account Type: {AccType}
-            """)
+                """)
+            proxynum = proxynum+1
